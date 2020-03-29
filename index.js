809185e9c2a2bf61687b3ccd388b86d687aea80d
mitsuoysharag <mitsuoysharag@gmail.com>
2020-03-29 18:33:16 -0500

    deploy

---
+++ index.js
@@ -0,0 +1,131 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+// Routing
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Starts the server.
+server.listen(5000, function () {
+  console.log('Starting server on port 5000');
+});
+
+// Add the WebSocket handlers
+let players = {};
+let player_velocity = 4;
+let ball_velocity = 8;
+let width = 1100;
+let height = 700;
+
+class Player {
+  constructor(pos, dir, vel, lim, ball) {
+    this.pos = pos
+    this.dir = dir
+    this.vel = vel
+    this.lim = lim
+    this.ball = ball
+  }
+  update() {
+    //Kick
+    var disX = this.pos.x - this.ball.pos.x;
+    var disY = this.pos.y - this.ball.pos.y;
+    var dis = Math.sqrt(disX * disX + disY * disY);
+    if (dis < 40) {
+      disX /= dis
+      disY /= dis
+      this.ball.dir.x = -disX
+      this.ball.dir.y = -disY
+    } else {
+      //Movement
+      let newPosX = this.pos.x + this.dir.x * this.vel
+      let newPosY = this.pos.y + this.dir.y * this.vel
+      let offset = 20
+
+      if (newPosX > width - offset || newPosX < offset) {
+        this.dir.x *= 0
+      }
+      if (newPosY > height - offset || newPosX < offset) {
+        this.dir.y *= 0
+      }
+
+      this.pos.x += this.dir.x * this.vel
+      this.pos.y += this.dir.y * this.vel
+    }
+  }
+}
+
+class Ball {
+  constructor(pos, dir, vel, lim) {
+    this.pos = pos
+    this.dir = dir
+    this.vel = vel
+    this.lim = lim
+  }
+  update() {
+    let newPosX = this.pos.x + this.dir.x * this.vel
+    let newPosY = this.pos.y + this.dir.y * this.vel
+    let offset = 10
+
+    if (newPosX > width - offset || newPosX < offset) {
+      this.dir.x *= -1
+    }
+    if (newPosY > height - offset || newPosY < offset) {
+      this.dir.y *= -1
+    }
+
+    this.pos.x += this.dir.x * this.vel
+    this.pos.y += this.dir.y * this.vel
+  }
+}
+
+let ball = new Ball({
+  x: width / 2,
+  y: height / 2
+}, {
+  x: 0,
+  y: 0
+}, ball_velocity, {
+  width,
+  height
+})
+
+io.on('connection', function (socket) {
+  socket.on('new player', function () {
+    players[socket.id] = new Player({
+      x: width / 2 - 100,
+      y: height / 2
+    }, {
+      x: 0,
+      y: 0
+    }, player_velocity, {
+      width,
+      height
+    },
+      ball)
+  });
+  socket.on('movement', function (dir) {
+    var player = players[socket.id] || {};
+    player.dir = dir
+  });
+  socket.on('disconnect', function () {
+    delete players[socket.id]
+  });
+});
+
+setInterval(function () {
+  ball.update()
+  Object.entries(players).forEach(([id, player]) => {
+    player.update()
+  });
+  io.sockets.emit('state', { players, ball });
+}, 1000 / 60);
---
+++ static/game.js
@@ -0,0 +1,64 @@
+var socket = io();
+
+var dir = {
+  x: 0,
+  y: 0
+}
+document.addEventListener('keydown', function (event) {
+  switch (event.keyCode) {
+    case 65: // A
+      dir.x = -1;
+      break;
+    case 87: // W
+      dir.y = -1;
+      break;
+    case 68: // D
+      dir.x = 1;
+      break;
+    case 83: // S
+      dir.y = 1;
+      break;
+  }
+});
+document.addEventListener('keyup', function (event) {
+  switch (event.keyCode) {
+    case 65: // A
+      dir.x = 0;
+      break;
+    case 87: // W
+      dir.y = 0;
+      break;
+    case 68: // D
+      dir.x = 0;
+      break;
+    case 83: // S
+      dir.y = 0;
+      break;
+  }
+});
+
+socket.emit('new player');
+setInterval(function () {
+  socket.emit('movement', dir);
+}, 1000 / 60);
+
+//Canvas
+var canvas = document.getElementById('canvas');
+canvas.width = 1100;
+canvas.height = 700;
+var context = canvas.getContext('2d');
+
+socket.on('state', ({ players, ball }) => {
+  context.clearRect(0, 0, 1100, 700);
+  context.fillStyle = 'green';
+  for (var id in players) {
+    var player = players[id];
+    context.beginPath();
+    context.arc(player.pos.x, player.pos.y, 20, 0, 2 * Math.PI);
+    context.fill();
+  }
+  context.fillStyle = 'black';
+  context.beginPath();
+  context.arc(ball.pos.x, ball.pos.y, 12, 0, 2 * Math.PI);
+  context.fill();
+});
